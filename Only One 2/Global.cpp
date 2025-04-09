a99f3d523f37d854a064bf2b25db8c9c672baa29
HUY-TD16 <24021521@vnu.edu.vn>
2025-04-09 18:57:54 +0700

    Create Global.cpp

---
+++ Only One 2/Global.cpp
@@ -0,0 +1,57 @@
+#pragma once
+#include "global.h"
+#include <iostream>
+
+SDL_Renderer* gRenderer = nullptr;
+SDL_Window* gWindow = nullptr;
+TTF_Font* gFont = NULL;
+
+void logErrorAndExit(const char* msg, const char* error) {
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+	SDL_Quit();
+}
+bool init() {
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		logErrorAndExit("SDL_Init", SDL_GetError());
+		return false;
+	}
+	gWindow = SDL_CreateWindow("Only One", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (!gWindow) {
+		logErrorAndExit("SDL_CreateWindow", SDL_GetError ());
+		return false;
+	}
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	if (!gRenderer) {
+		logErrorAndExit("Create REnder", SDL_GetError());
+		return false;
+	}
+	gFont = TTF_OpenFont("Font/lazy.ttf", 28);
+	if (TTF_Init() == -1)
+	{
+		printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+		return false;
+	}
+	gFont = TTF_OpenFont("Font/lazy.ttf", 28);
+	if (gFont == NULL) {
+		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+		return false;
+	}
+	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	int imgFlag = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlag) & imgFlag)) {
+		logErrorAndExit("imgFlag" , SDL_GetError());
+		return false ;
+	}
+	return true;
+}
+void close() {
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
+}
+bool isButtonClick(int x, int y, SDL_Rect button) {
+	return (x >= button.x && x <= button.x + button.w && y >= button.y && y <= button.y + button.h);
+}
