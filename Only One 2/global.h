3dec691f33a724c5e2faf1d1c80e7e2391d1cac5
HUY-TD16 <24021521@vnu.edu.vn>
2025-04-09 18:57:51 +0700

    Create global.h

---
+++ Only One 2/global.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include "LTexture.h"
+#include <SDL_ttf.h>
+
+// mÀN HÌNH 
+extern SDL_Window* gWindow ;
+extern SDL_Renderer* gRenderer ;
+extern TTF_Font* gFont;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+// Kích thước và tốc độ 
+const int PLAYER_VEL = 3  ;
+const int PLAYER_SIZE = 20;
+const int FIREBALL_SIZE = 20;
+const float FIREBALL_SPEED = 200.00f;
+const int DASHDISTANCE = 100;
+
+// Thời gian
+const Uint32 frameDelay = 100;
+const Uint32 increaseInterval = 20000;
+
+void updateFrameTime();
+bool init();
+void close();
+void logErrorAndExit(const char* msg, const char* error);
+bool isButtonClick(int x, int y, SDL_Rect button);
+
