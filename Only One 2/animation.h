fa94a331f7abe10e8af68474f18f172e3634b18c
HUY-TD16 <24021521@vnu.edu.vn>
2025-04-09 18:58:32 +0700

    Create animation.h

---
+++ Only One 2/animation.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include "global.h"
+#include "player.h"
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+enum class Action {
+	IDLE,
+	RUN
+};
+enum class Direction {
+	FRONT,
+	BACK,
+	RIGHT,
+	LEFT
+};
+class Animation {
+public:
+	Animation();
+	~Animation();
+
+	bool loadSpritesheet(SDL_Renderer* gRenderer, const std::string path, Action action , Direction direction , int frameCount );
+	void update();
+	void render(SDL_Renderer* gRenderer, int x, int y);
+	void setAction(Action action);
+	void setDirection(Direction direction);
+	void handleInput(SDL_Event& e);
+	void updateAnimationState();
+private:
+	struct FrameInfo {
+		SDL_Texture* gTexture;
+		int frameCount;
+	};
+	std::map<std::pair<Action, Direction> , FrameInfo> animationFrames;
+	Action currentAction;
+	Direction currentDirection;
+
+	int currentFrame;
+	int frameWidth;
+	int frameHeight;
+	Uint32 lastUpdateTime;
+	int frameDelay;
+
+	std::set<SDL_Keycode> keyState;
+
+};
