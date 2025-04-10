da25491e5e7c855069d9168b2048c44b6aedf17e
HUY-TD16 <24021521@vnu.edu.vn>
2025-04-10 07:02:37 +0700

    Update game.h
    update logic game

--- Only One 2/game.h
+++ Only One 2/game.h
@@ -1,54 +1,54 @@
-//#pragma once
-//
-//#include <SDL.h>
-//#include <SDL_ttf.h>
-//#include <vector>
-//#include <string>
-//#include "player.h"
-//#include "skill.h"
-//#include "global.h"
-//#include "fireball.h"
-//#include "lasers.h"
-//#include "explosions.h"
-//#include "collision.h"
-//#include "interface.h"
-//enum class State {
-//    MENU,
-//    PLAYING,
-//    PAUSED,
-//    GAME_OVER
-//};
-//class Game {
-//public:
-//    Game();
-//    ~Game();
-//    void update(float deltaTime, Uint32 currentTime);
-//    void render();
-//    void handleInput(SDL_Event& event);
-//    void resetGame();
-//    void loadHighScores();
-//    void saveHighScores();
-//private:
-//    Player player;
-//    Skills skill;
-//    FireBall fireball;
-//    Lasers laser;
-//    Explosion explosion;
-//    Collision collsision;
-//    Interface interface;
-//    std::vector<ClearEffect> clearEffect;
-//    //Time
-//    Uint32 gameStartTime;
-//    Uint32 lastSpawnTime;
-//    Uint32 lastFrameTime;
-//    Uint32 lastLaserTime;
-//    Uint32 lastExplosionTime;
-//    Uint32 finalTime;
-//    State state;
-//    std::vector<int> highScore;
-//    bool isNewHighScore;
-//    bool showTutorial;
-//    bool showHighScore;
-//    bool soundOn;
-//
-//};
+#pragma once
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <vector>
+#include <string>
+#include "player.h"
+#include "skill.h"
+#include "global.h"
+#include "fireball.h"
+#include "lasers.h"
+#include "explosions.h"
+#include "collision.h"
+#include "interface.h"
+enum class State {
+    MENU,
+    PLAYING,
+    PAUSED,
+    GAME_OVER
+};
+class Game {
+public:
+    Game();
+    ~Game();
+    void update(float deltaTime, Uint32 currentTime);
+    void render();
+    void handleInput(SDL_Event& event);
+    void resetGame();
+    void loadHighScores();
+    void saveHighScores();
+private:
+    Player player;
+    Skills skill;
+    FireBall fireball;
+    Lasers laser;
+    Explosion explosion;
+    Collision collsision;
+    Interface interface;
+    std::vector<ClearEffect> clearEffect;
+    //Time
+    Uint32 gameStartTime;
+    Uint32 lastSpawnTime;
+    Uint32 lastFrameTime;
+    Uint32 lastLaserTime;
+    Uint32 lastExplosionTime;
+    Uint32 finalTime;
+    State state;
+    std::vector<int> highScore;
+    bool isNewHighScore;
+    bool showTutorial;
+    bool showHighScore;
+    bool soundOn;
+
+};
